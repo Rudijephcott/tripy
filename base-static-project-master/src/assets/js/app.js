068d1f350f91e250a9692f62d40f2a937281ac82
Rudi Jephcott <rudijephcott@gmail.com>
2022-08-21 22:25:51 +0200

    added page scroll + stats

--- base-static-project-master/src/assets/js/app.js
+++ base-static-project-master/src/assets/js/app.js
@@ -1,3 +1,25 @@
+
+document.addEventListener("keyup", function (e) {
+    if (e.keyCode === 71) {
+      document.querySelector(".grid-overlay").classList.toggle("show");
+    }
+  });
+  
+  $(document).ready(function() {
+    $('.nav-bar a').click(function(event) {
+      let targetElement = $(this.hash);
+      if (targetElement.length) {
+      event.preventDefault();
+      let targetCalc = targetElement.offset().top - 50;
+      $('html, body').animate({
+        scrollTop: targetCalc
+      }, 1000);
+    }});
+  
+  });
+
+
+
 // (function() {
 
 //     const target = document.querySelector(".target");
